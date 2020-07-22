fafe76c71e1ebeca388dfa6d5dd67fd4c5e64bc2
renewss <2ibragimov01@gmail.com>
2020-07-22 20:38:18 +0500

    change mail text

--- src/mailing.js
+++ src/mailing.js
@@ -14,14 +14,16 @@ module.exports = async (bot) => {
         users.forEach(async (user) => {
             user.diffs.forEach(async (el) => {
                 //finding difference
-                let compare;
+                let absDiff, realDiff;
                 if (el.name === 'value') {
-                    compare = Math.abs(currentData[el.crypt][el.fiat] - el.rate);
+                    realDiff = currentData[el.crypt][el.fiat] - el.rate;
+                    absDiff = Math.abs(realDiff);
                 } else {
-                    compare = Math.abs(((currentData[el.crypt][el.fiat] - el.rate) / el.rate) * 100);
+                    realDiff = ((currentData[el.crypt][el.fiat] - el.rate) / el.rate) * 100;
+                    absDiff = Math.abs(realDiff);
                 }
 
-                if (el.ranges[0] <= compare && el.ranges[1] >= compare) {
+                if (el.ranges[0] <= absDiff && el.ranges[1] >= absDiff) {
                     // updating db value
                     await Diff.findOneAndUpdate({ _id: el._id }, { rate: currentData[el.crypt][el.fiat] });
 
@@ -29,11 +31,13 @@ module.exports = async (bot) => {
 
                     bot.telegram.sendMessage(
                         user.chatId,
-                        `Hey, there was change in range ${el.ranges[0]} and ${el.ranges[1]} ${symbol}\nCurrent: ${
+                        `Change ${symbol}\nRange: </strong>${el.ranges[0]} and ${
+                            el.ranges[1]
+                        } ${symbol}<strong>\nCurrent: ${
                             Math.round(currentData[el.crypt][el.fiat] * 100) / 100
                         } | Previous: ${Math.round(el.rate * 100) / 100}\nDifference:  <strong>${
-                            Math.round(compare * 100) / 100
-                        }</strong> ${symbol}\nfor <strong>${el.crypt}</strong>`,
+                            Math.round(realDiff * 100) / 100
+                        } ${el.crypt}</strong>`,
 
                         {
                             parse_mode: 'HTML',
