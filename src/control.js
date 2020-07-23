c69c122b1f82cbb34c8082815ba9940ea3872020
renewss <2ibragimov01@gmail.com>
2020-07-23 22:36:21 +0500

    add new crypts

--- src/control.js
+++ src/control.js
@@ -4,7 +4,7 @@ const fetch = require('./fetcher');
 const utils = require('./utils');
 const { findOne } = require('./model');
 
-const crypts = ['BTC', 'USDT'];
+const crypts = ['BTC', 'USDT', 'ETH', 'BAT'];
 const fiats = ['RUB', 'USD', 'EUR'];
 
 exports.auth = async (ctx, next) => {
